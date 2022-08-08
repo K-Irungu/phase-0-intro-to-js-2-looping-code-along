d28e78938f85c2a32430892a363b41ff26f46083
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-08-08 21:34:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+
+// Thank You card creator function
+  const names = ["Charlie", "Samip", "Ali", "Guadalupe", "Ollie", "Aki"]; // The current array
+  const thankYouNotes = []; // The new array to be filled
+  const eventName = "birthday";
+
+  function writeCards(names, eventName) {
+  for (let i = 0; i < names.length; i++) { 
+      thankYouNotes.push((`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`));
+  }
+
+  return thankYouNotes;
+
+  }
+
+// The countdown function
+
+function countDown(num) {
+    while (num > -1) {
+        console.log(num--);
+}
+
+}
+
+countDown(10);
+
+
